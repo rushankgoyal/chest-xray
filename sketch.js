8f85a2886ef96670489cb5c0d82c2eb908c7032c
rushankgoyal <60558881+rushankgoyal@users.noreply.github.com>
2021-03-06 18:06:43 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,21 +1,14 @@
-let video, classifier, label = "waiting...";
+let classifier;
 
 function preload() {
     classifier = ml5.imageClassifier("https://storage.googleapis.com/tm-model/W4qRx-0aF/model.json")
 }
 
-function setup() {
-    createCanvas(640, 520), (video = createCapture(VIDEO)).hide(), classifyVideo()
-}
-
-function classifyVideo() {
-    classifier.classify(video, gotResults)
-}
-
-function draw() {
-    background(0), image(video, 0, 0), textSize(32), textAlign(CENTER, CENTER), fill(255), text(label, width / 2, height - 16)
+function classifyImg() {
+    var x = document.forms["myForm"]["image"].value;
+    classifier.classify(x, gotResults)
 }
 
 function gotResults(e, i) {
-    e ? console.error(e) : label = i[0].label
+    e ? console.error(e) : alert(i[0])
 }
