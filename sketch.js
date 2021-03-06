d0ae762673209b26f71f153fc0d193592757dabf
rushankgoyal <60558881+rushankgoyal@users.noreply.github.com>
2021-03-06 19:09:48 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,15 @@
 let classifier;
 
 function preload() {
-    classifier = ml5.imageClassifier("https://storage.googleapis.com/tm-model/W4qRx-0aF/model.json")
+    classifier = ml5.imageClassifier("https://storage.googleapis.com/tm-model/W4qRx-0aF/model.json");
 }
 
 function classifyImg() {
     var x = document.forms["myForm"]["image"].value;
-    classifier.classify(x, gotResults)
+    alert(x)
+    classifier.classify(x, gotResults);
 }
 
 function gotResults(e, i) {
-    e ? console.error(e) : alert(i[0])
+    alert(i[0]);
 }
